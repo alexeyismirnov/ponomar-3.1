47db36b35bb58f47fcdadcc5c2e8fe7835adec99
Alexey Smirnov <alexey@gmail.com>
2024-08-14 22:18:22 +0800

    JSON.dateParser fixed and more epubs

--- lib/saints_lives.dart
+++ lib/saints_lives.dart
@@ -34,6 +34,10 @@ class SaintsCalendar {
     List<Map<String, Object?>> query =
         await db.query("content", columns: ["text"], orderBy: "title");
 
+    // prepare for JSON parsing
+    final cal = ChurchCalendar.fromDate(DateTime.utc(year, 1, 1));
+    JSON.dateParser = cal.dateParser;
+
     days =
         query.map<ChurchDay>((e) => ChurchDay.fromJson(jsonDecode(e["text"] as String))).toList();
 
